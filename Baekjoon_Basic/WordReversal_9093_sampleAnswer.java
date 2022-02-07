3f87df36f87cfd25ce1c6cf4ba93f46c0b5e54be
shersky123 <shersky007@gmail.com>
2022-02-07 21:17:15 +0900

    Create WordReversal_9093_sampleAnswer.java
    단어 뒤집기 9093 예시 답안(라이브러리 사용)

---
+++ Baekjoon_Basic/WordReversal_9093_sampleAnswer.java
@@ -0,0 +1,26 @@
+package algorithm;
+
+import java.util.*;
+import java.io.*;
+public class WordReversal_9093_sampleAnswer {
+	public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(bf.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        while (t-- > 0) {
+            String str = bf.readLine() + "\n";
+            Stack<Character> s = new Stack<>();
+            for (char ch : str.toCharArray()) {
+                if (ch == '\n' || ch == ' ') {
+                    while (!s.isEmpty()) {
+                        bw.write(s.pop());
+                    }
+                    bw.write(ch);
+                } else {
+                    s.push(ch);
+                }
+            }
+        }
+        bw.flush();
+    }
+}
